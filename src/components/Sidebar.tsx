47680ae3f2586a41f29714ed1d6685cd53431c9f
Yoga <yogabudiman07@gmail.com>
2024-06-11 22:36:49 +0700

    style: sidebar layout for desktop and tablet size

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,38 +1,38 @@
 function Sidebar() {
   return (
-    <div className="flex-1 flex lg:justify-between lg:flex-col lg:max-w-[18.75rem] bg-white border-r-2 border-ice-blue">
-      <div className="flex flex-col lg:pr-8 lg:gap-5">
-        <h3 className="uppercase font-bold text-medium-grey lg:text-xs lg:tracking-[0.2em] lg:pl-8">
+    <div className="flex-1 flex md:justify-between md:flex-col md:max-w-[16.375rem] lg:max-w-[18.875rem] bg-white border-r-2 border-ice-blue">
+      <div className="flex flex-col md:pr-5 lg:pr-8 md:mt-[0.9375rem] md:gap-5 ">
+        <h3 className="uppercase font-bold text-medium-grey md:text-xs md:tracking-[0.2em] md:pl-6 lg:pl-8">
           All Boards
         </h3>
 
         <ul>
-          <li className="bg-primary-purple lg:py-4 lg:rounded-tr-full lg:rounded-br-full">
-            <button className="font-bold text-white lg:text-[0.9375rem] lg:pl-8">
+          <li className="bg-primary-purple md:py-4 md:rounded-tr-full md:rounded-br-full">
+            <button className="font-bold text-white lg:text-[0.9375rem] md:pl-6 lg:pl-8">
               Platform Launch
             </button>
           </li>
-          <li className="lg:py-4">
-            <button className="font-bold text-medium-grey bold lg:text-[0.9375rem] lg:pl-8">
+          <li className="md:py-4">
+            <button className="font-bold text-medium-grey bold lg:text-[0.9375rem] md:pl-6 lg:pl-8">
               Marketing Plan
             </button>
           </li>
-          <li className="lg:py-4 ">
-            <button className="font-bold text-medium-grey bold lg:text-[0.9375rem] lg:pl-8">
+          <li className="md:py-4 ">
+            <button className="font-bold text-medium-grey bold lg:text-[0.9375rem] md:pl-6 lg:pl-8">
               Roadmap
             </button>
           </li>
-          <li className="lg:py-4">
-            <button className="font-bold text-medium-grey bold lg:text-[0.9375rem] lg:pl-8">
+          <li className="md:py-4">
+            <button className="font-bold text-medium-grey bold lg:text-[0.9375rem] md:pl-6 lg:pl-8">
               + Create New Board
             </button>
           </li>
         </ul>
       </div>
 
-      <div className="flex flex-col lg:mb-8">
-        <div className="lg:px-6 lg:mb-2">
-          <div className="bg-light-grey lg:text-center lg:py-[0.875rem]">
+      <div className="flex flex-col md:mb-8">
+        <div className="md:px-3 lg:px-6 lg:mb-2">
+          <div className="bg-light-grey md:text-center md:py-[0.875rem] md:mb-4 lg:mb-2">
             <input type="checkbox"></input>
             <label htmlFor="">Darkmode</label>
           </div>
