f4659812101ce4331b6cced8fb793f476c691e70
Yoga <yogabudiman07@gmail.com>
2024-06-16 18:08:07 +0700

    style: additional boards and taskCards styling. Adding new column and styling

--- src/Boards.tsx
+++ src/Boards.tsx
@@ -3,19 +3,11 @@ import data from "../data.json";
 import TaskCard from "./components/TaskCard";
 
 function Boards() {
-  const [colAmount, setColAmount] = useState(
-    data["boards"][0]["columns"].length
-  );
-
-  const colsAmountClass = `grid-cols-${colAmount}`;
-
   return (
     <div className="flex-1 bg-light-grey">
-      <div
-        className={`grid grid-cols-3 md:gap-6 overflow-x-auto lg:px-6 lg:pt-6 lg:pb-[3.125rem]`}
-      >
+      <div className="flex md:gap-6 overflow-x-auto lg:px-6 lg:pt-6 lg:pb-[3.125rem]">
         {data["boards"][0]["columns"].map((column, idx) => (
-          <div className="" key={idx}>
+          <div className="flex-1 md:max-w-[17.5rem]" key={idx}>
             <h3 className="uppercase font-bold text-medium-grey md:text-xs md:tracking-[0.2em] md:mb-6">
               {column.name}
             </h3>
@@ -27,6 +19,11 @@ function Boards() {
             </ul>
           </div>
         ))}
+        <div className="flex-1 rounded-md grid place-content-center bg-ice-blue">
+          <button className="font-bold text-2xl text-medium-grey">
+            + New Column
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,7 +1,10 @@
 function TaskCard({ task }) {
   return (
-    <li className="bg-white rounded-lg md:px-4 md:py-6">
-      <p className="font-bold text-[0.9375rem]">{task.title}</p>
+    <li className="bg-white rounded-lg shadow-md md:px-4 md:py-6">
+      <p className="font-bold text-[0.9375rem] md:mb-2">{task.title}</p>
+      <p className="text-medium-grey font-bold text-xs">
+        Lorem ipsum, dolor sit
+      </p>
     </li>
   );
 }
