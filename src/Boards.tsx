a0365757f4a3604ac304911cf89b12fb3f0799b9
Yoga <yogabudiman07@gmail.com>
2024-06-16 20:26:08 +0700

    style: add overflow to boards

--- src/Boards.tsx
+++ src/Boards.tsx
@@ -5,9 +5,9 @@ import TaskCard from "./components/TaskCard";
 function Boards() {
   return (
     <div className="flex-1 bg-light-grey">
-      <div className="flex md:gap-6 overflow-x-auto lg:px-6 lg:pt-6 lg:pb-[3.125rem]">
+      <div className="flex overflow-x-auto md:gap-6 lg:px-6 lg:pt-6 lg:pb-[3.125rem]">
         {data["boards"][0]["columns"].map((column, idx) => (
-          <div className="flex-1 md:max-w-[17.5rem]" key={idx}>
+          <div className="flex-[1_0_auto] md:max-w-[17.5rem] w-full" key={idx}>
             <h3 className="uppercase font-bold text-medium-grey md:text-xs md:tracking-[0.2em] md:mb-6">
               {column.name}
             </h3>
@@ -19,7 +19,8 @@ function Boards() {
             </ul>
           </div>
         ))}
-        <div className="flex-1 rounded-md grid place-content-center bg-ice-blue">
+
+        <div className="flex-[1_0_auto] bg-ice-blue md:max-w-[17.5rem] w-full rounded-md grid place-content-center ">
           <button className="font-bold text-2xl text-medium-grey">
             + New Column
           </button>
